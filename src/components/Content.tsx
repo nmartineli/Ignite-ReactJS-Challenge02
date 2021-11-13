47fc7d116f6ed5b017bec8a7a8449d9fcdd871d5
Natalia Martineli <nataliamartineli@gmail.com>
2021-11-13 17:05:33 -0300

    Refactored Content Component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  movies: [
+    {
+      imdbID: string;
+      Title: string;
+      Poster: string;
+      Ratings: Array<{
+        Source: string;
+        Value: string;
+      }>;
+      Runtime: string;
+    }
+  ];
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <main>
+      <div className="movies-list">
+        {props.movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
